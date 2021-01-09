d72fd97b9d47adb2789a82caf7588fad0b0a578c
yakulbeans <yakulbeans@gmail.com>
2021-01-08 22:24:03 -0700

    Added slight formatting for header

--- 1.3/header.c
+++ 1.3/header.c
@@ -3,15 +3,22 @@
 int main(void) {
     float fahr, celsius;
     int lower, upper, step;
-
+    
     lower = 0;
     upper = 300;
     step = 20;
+    char *f = "farenheit";
+    char *c = "celsius";
 
     fahr = lower;
+    
+    printf("\n-FARENHEIT TO CELSIUS CONVERTER-\n");
+    printf("\n%s", f);
+    printf("%14s\n", c);
+
     while (fahr <= upper) {
 	celsius = (5.0/9.0) * (fahr - 32.0);
-	printf("%3.0f %6.1f\n", fahr, celsius);
+	printf("%9.0f %13.1f\n", fahr, celsius);
 	fahr = fahr + step;
     }
 }
