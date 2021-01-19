8cd5a3c6ee2f7dc09c4b0f72a624dc4aeb04a492
yakulbeans <yakulbeans@gmail.com>
2021-01-18 22:14:48 -0700

    added 1.5

---
+++ 1.5/reverse.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("FAHR TO CELSIUS CONVERTER\n");
+	printf("Farenheit\tCelsius\n");
+    for (int fahr = 300; fahr >= 0; fahr -= 20) {
+        printf("%9d %13.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+    }
+}
