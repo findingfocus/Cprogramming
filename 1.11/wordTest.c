a48dd8f644dc3216529748baa9db7d1ec274f532
yakulbeansMacLaptop <yakulbeans@gmail.com>
2022-04-23 12:13:32 -0600

    Added exercise 1.11

---
+++ 1.11/wordTest.c
@@ -0,0 +1,12 @@
+/* What kinds of input are most likely to uncover bugs in wordcout program?
+
+Spaces, lines, tabs, and backspaces are probably most prone to bugs
+
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    return 0;  
+}
